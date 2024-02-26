9488fec0a17a6e8adf01719e5ac50677b4383ab2
Sho Kiami <kiami.sho@gmail.com>
2024-02-26 14:26:48 -0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+# clone epigraph
 git clone git@github.com:EmbersArc/Epigraph.git
-cd Epigraph
-mkdir build
-cmake --build build
-cd ..
+
+# run cmake
+mkdir Epigraph/build
+cmake -S Epigraph -B Epigraph/build -DENABLE_ECOS=TRUE -DENABLE_OSQP=True
+cmake --build Epigraph/build
+
+# copy libraries
 cp Epigraph/build/libepigraph.so libs
 cp Epigraph/build/solvers/ecos/libecos.so libs
 cp Epigraph/build/solvers/osqp/out/libosqp.so libs
